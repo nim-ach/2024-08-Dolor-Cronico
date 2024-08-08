8d0897b51c21e0b1cf2c1c8f3316535cd986c8da
Matías Castillo <matcasti@umag.cl>
2024-08-08 13:07:31 -0300

    script para ejecutar modelos

---
+++ R/_fit_models.R
@@ -0,0 +1,17 @@
+## Factores sociodemograficos en el dolor
+source("R/2.1-factores-y-dolor-funcional.R")
+source("R/2.2-factores-y-dolor-escala.R")
+source("R/2.3-factores-y-dolor-frecuencia.R")
+source("R/2.4-factores-y-dolor-duracion.R")
+
+## Actividad física y dolor
+source("R/3.1-af-dolor-funcional.R")
+source("R/3.2-af-y-dolor-escala.R")
+source("R/3.3-af-y-dolor-frecuencia.R")
+source("R/3.4-af-y-dolor-duracion.R")
+
+## Calidad de sueño y dolor
+source("R/4.1-psqi-dolor-funcional.R")
+source("R/4.2-psqi-y-dolor-escala.R")
+source("R/4.3-psqi-y-dolor-frecuencia.R")
+source("R/4.4-psqi-y-dolor-duracion.R")
